8a2ac4b5723eb7281c724a8c67958788c33ea5e2
Piyush Pant <piyushpant17@gmail.com>
2019-08-05 22:22:11 +0530

    Added Update servlet

---
+++ src/java/backend/Update.java
@@ -0,0 +1,112 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package backend;
+
+import static backend.HashPass.md5;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author HP
+ */
+@WebServlet(name = "Update", urlPatterns = {"/Update"})
+public class Update extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            try {
+                Class.forName("com.mysql.jdbc.Driver");
+                Connection conn = DriverManager.getConnection(
+                        "jdbc:mysql://localhost:3306/hallbooking?zeroDateTimeBehavior=convertToNull",
+                        "root","");
+                HttpSession session = request.getSession(false);
+                String usernameSess = session.getAttribute("username").toString();
+                String username = request.getParameter("username");
+                //String password = request.getParameter("password");
+                //password = md5(password);
+                String email = request.getParameter("email");
+                String secq = request.getParameter("secq");
+                String answer = request.getParameter("ans");
+                String query = "update login set username = '"+username+"',email='"+email+"',securityquestion='"+secq
+                        +"',answer='"+answer+"' where username='"+usernameSess+"'";
+                
+                PreparedStatement smt = conn.prepareStatement(query);
+                int updateStatus = smt.executeUpdate();
+                if (updateStatus > 0) {
+                    out.println("Details Updated Successfully");
+                } else {
+                    out.println("Error in Updating Details");
+                }
+                conn.close();
+            } catch (Exception e) {
+                out.print(e.getMessage());
+            }
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+}
